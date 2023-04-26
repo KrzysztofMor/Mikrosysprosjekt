941f224a3bd8fbe0dd777ece588aac123e4a5feb
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-04-26 22:14:56 +0200

    Update main.c
    kommentert kode

--- RTC & eeprom.X/main.c
+++ RTC & eeprom.X/main.c
@@ -15,26 +15,25 @@
 #include "RTC.h"
 
 
-
-
 int main() {
     USART3_init();
-    PORTB_DIRCLR = PIN2_bm;
-    RTC_init();
-    fan_memory();
-    sei();
+    PORTB_DIRCLR = PIN2_bm; //A button can be used to reset runtime.
+    RTC_init(); //Starts counter
+    fan_memory();   //reads stored fan runtime from eeprom
+    sei();  //enables interrupts 
 
     while(1){
-        uint8_t x = get_runtime_fan(0);
-        printf("fan1: %u\n", x);
-        _delay_ms(1000);
+        uint8_t x = get_runtime_fan(0); //get runtime from one of the fans
+        printf("fan1: %u\n", x);    //serialprint runtime
+        _delay_ms(1000);  
         
         //reset_fan_runtime(0);         //Reset fancount when fan is replaced
         
-        if (!(PORTC.IN & (PIN2_bm))) {
+        if (!(PORTC.IN & (PIN2_bm))) {  //Use to enable reset on buttonpress
         //    reset_fan_runtime(0);
         }
     }
     
     
 }
+
