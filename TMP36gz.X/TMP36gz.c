6034c33f3fee5ca9d2b48274ac14a07618620eb4
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-04-26 22:52:38 +0200

    Update TMP36gz.c
    kommentert

--- TMP36gz.X/TMP36gz.c
+++ TMP36gz.X/TMP36gz.c
@@ -12,9 +12,8 @@ uint16_t analogTempvalue(uint16_t temperature, uint16_t min, uint16_t max);
 long map ( long x , long in_min , long in_max , long out_min , long out_max );
 
 //case 0 is Celsius, case 1 is Fahrenheit, case 2 is Kelvin
-
 uint16_t temperaturC(uint16_t temp, uint8_t tempScale){
-    uint8_t c = (temp-500)/10;
+    uint8_t c = (temp-500)/10;  //equation for TMP36gz sensor. Value is celcius.
     switch(tempScale){
         case 0:
         temp = c;
@@ -32,6 +31,11 @@ uint16_t temperaturC(uint16_t temp, uint8_t tempScale){
     return temp;
 }
 
+/*
+ This function takes in temperature value and convert it to a analog value.
+ Returns the temperature in analog scale, if temperature is above minimum or
+ maximum value.
+ */
 uint16_t analogTempvalue(uint16_t temperature, uint16_t min, uint16_t max){
         if(temperature > min){
             if(temperature > max) {
@@ -44,6 +48,9 @@ uint16_t analogTempvalue(uint16_t temperature, uint16_t min, uint16_t max){
         }
 }
 
+/*
+ map converts a value from one scale, over to a different scale.
+ */
 long map ( long x , long in_min , long in_max , long out_min , long out_max ) {
     return (x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min ;
 }  
