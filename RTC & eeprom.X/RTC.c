8d96ebd7936fe37872baf2724c3e07714782506c
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-04-26 22:08:31 +0200

    Update RTC.c
    Kommentert så det blir klarere hvordan funksjonene brukes

--- RTC & eeprom.X/RTC.c
+++ RTC & eeprom.X/RTC.c
@@ -5,12 +5,12 @@
 #include <stdbool.h>
 #include "RTC.h"
 
+#define one_second 0x3ff
+#define Fancount 8
+int hour = 3600;
+uint16_t Fantime[Fancount];
 volatile uint64_t runtime_seconds = 0;
 volatile uint64_t pretime = 0;
-#define one_second 0x3ff
-int hour = 3600;
-#define Fancount 8
-uint16_t Fantime[Fancount];
 
 ISR(RTC_CNT_vect){
     ++runtime_seconds;
@@ -18,6 +18,11 @@ ISR(RTC_CNT_vect){
     RTC.INTFLAGS = RTC_OVF_bm;
 }
 
+//////////////////Files that need to be used in main/////////////////////////
+
+/*
+ Initialisate RTC timer so that a interrupt occurs every secound 
+ */
 void RTC_init(){
     RTC.CTRLA = 1 << RTC_RTCEN_bp;     
     RTC.PER = one_second; 
@@ -25,19 +30,10 @@ void RTC_init(){
     RTC.INTCTRL = 1 << RTC_OVF_bp;
 }
 
-bool delayTime(uint8_t time_in_s){
-    uint64_t meantime = runtime_seconds;
-    if((meantime - pretime) > time_in_s){
-        
-        pretime = runtime_seconds;
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-
+/*
+ Returns stored timervalue from a array. Every hour this value is changed.
+ Choose which fan you want to read runtime by initialize a number from 0 to 7. 
+ */
 uint16_t get_runtime_fan(uint8_t fan){
     uint16_t f = 0;
     switch(fan){
@@ -69,18 +65,10 @@ uint16_t get_runtime_fan(uint8_t fan){
     return f;
 }
 
-void enable_fan_runtime(){
-    if(delayTime(10) == true){ 
-        for(int i=0; i < Fancount; ++i){
-            if(eeprom_is_ready()){
-                Fantime[i]++;
-                eeprom_write_byte((uint8_t*)i, Fantime[i]);
-            }
-        }
-    }
-    
-}
-
+/*
+ Reads stored value from EEprom and writes into Fantime[]. This should be run
+ on startup to restore timer values.
+ */
 void fan_memory(){
     for(int i=0; i < Fancount; ++i){
             Fantime[i] = 0;
@@ -92,6 +80,43 @@ void fan_memory(){
     }
 }
 
+/////////////Files that are only called in this C-file///////////////////////
+
+/*
+ Writes new values to Fancount[] every hour, and store these values to eeprom.
+ This function is run when a interrupt occurs. 
+ */
+void enable_fan_runtime(){
+    if(delayTime(hour) == true){ 
+        for(int i=0; i < Fancount; ++i){
+            if(eeprom_is_ready()){
+                Fantime[i]++;
+                eeprom_write_byte((uint8_t*)i, Fantime[i]);
+            }
+        }
+    }
+    
+}
+
+/*
+ Simple timer function that returns a boolean value. Initialize when called, and
+ if statement will return true when time is reached.
+ */
+bool delayTime(uint8_t time_in_s){
+    uint64_t meantime = runtime_seconds;
+    if((meantime - pretime) > time_in_s){        
+        pretime = runtime_seconds;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+/*
+ For every maintenence or switch of fans, the runtime should be reset. Call this
+ function and initialize which fan runtime should be reset. Takes values 0 to 7.
+ */
 void reset_fan_runtime(uint8_t fan){
     Fantime[fan] = 0;
 }
