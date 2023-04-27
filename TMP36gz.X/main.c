598370aa87688ea7881c5faba5599e5a9cf130b2
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-04-27 09:32:12 +0200

    Update main.c
    komprimert

--- TMP36gz.X/main.c
+++ TMP36gz.X/main.c
@@ -6,7 +6,6 @@
 */
 
 #define F_CPU 4000000UL
-//#define RTC_PERIOD      //RTC periode
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -17,22 +16,25 @@
 #include "usart.h"
 #include "adc.h"
 
+uint16_t readADC(uint16_t pin, uint16_t minimum, uint16_t maximum);
 
-int main(void) {
-    ADC0_init(3);   //Activates ADC on port D with initialized pin
-    USART3_init();  //Activates serial connection over PB0 and PB1
-	
+int main(void) {  
+    USART3_init();  //Activates serial connection over PB0 and PB1	
 	sei();
     
     while(1){
-        uint16_t adcVal = ADC0_read();  //Reads Analog value on input
-        uint16_t TemperaturRead = temperaturC(adcVal, 0);   //converts A-value to temperature
-        //printf("Grader:%d\r\n",TemperaturRead);
-        uint16_t analogValue = analogTempvalue(TemperaturRead, 0, 23);  //Converts Temperatur to analog value with minimum and maximum range.
+        readADC(0, 20, 100) //PD pin, min temp, max temp   
         printf("PWM:%d\r\n",analogValue);
         _delay_ms(1000);                          //Litt venting før neste verdi blir sendt 
         
     }
 }
 
+uint16_t readADC(uint16_t pin, uint16_t minimum, uint16_t maximum){
+    ADC0.init(pin); //Activates ADC on port D with initialized pin
+    uint16_t adcVal = ADC0_read();  //Reads Analog value on input
+    uint16_t TemperaturRead = temperaturC(adcVal, 0);   //converts A-value to temperature
+    uint16_t analogValue = analogTempvalue(TemperaturRead, minimum, maximum);   //Converts Temperatur to analog value with minimum and maximum range. 
+    return analogValue;
+}
 
