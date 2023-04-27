b934372847d25f39490da63ee995bd7bb1d19d84
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-04-27 11:02:20 +0200

    Update TMP36gz.h

--- TMP36gz.X/TMP36gz.h
+++ TMP36gz.X/TMP36gz.h
@@ -9,6 +9,8 @@
 
 uint16_t temperaturC(uint16_t temp, uint8_t tempScale);
 uint16_t analogTempvalue(uint16_t temperature, uint16_t min, uint16_t max);
+uint16_t read_i2c_temp(uint16_t pin, uint16_t minimum, uint16_t maximum);
+uint16_t read_fantemperature(uint16_t pin);
 
 
 
