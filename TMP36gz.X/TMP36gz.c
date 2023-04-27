b32a766b806012626cd6b11a79f79c3eccc55245
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-04-27 11:02:04 +0200

    Update TMP36gz.c

--- TMP36gz.X/TMP36gz.c
+++ TMP36gz.X/TMP36gz.c
@@ -10,6 +10,8 @@
 uint16_t temperaturC(uint16_t temp, uint8_t tempScale);
 uint16_t analogTempvalue(uint16_t temperature, uint16_t min, uint16_t max);
 long map ( long x , long in_min , long in_max , long out_min , long out_max );
+uint16_t read_i2c_temp(uint16_t pin, uint16_t minimum, uint16_t maximum);
+uint16_t read_fantemperature(uint16_t pin);
 
 //case 0 is Celsius, case 1 is Fahrenheit, case 2 is Kelvin
 uint16_t temperaturC(uint16_t temp, uint8_t tempScale){
@@ -54,3 +56,18 @@ uint16_t analogTempvalue(uint16_t temperature, uint16_t min, uint16_t max){
 long map ( long x , long in_min , long in_max , long out_min , long out_max ) {
     return (x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min ;
 }  
+
+uint16_t read_i2c_temp(uint16_t pin, uint16_t minimum, uint16_t maximum){
+    ADC0.init(pin); //Activates ADC on port D with initialized pin
+    uint16_t adcVal = ADC0_read();  //Reads Analog value on input
+    uint16_t TemperaturRead = temperaturC(adcVal, 0);   //converts A-value to temperature
+    uint16_t analogValue = analogTempvalue(TemperaturRead, minimum, maximum);   //Converts Temperatur to analog value with minimum and maximum range. 
+    return analogValue;
+}
+
+uint16_t read_fantemperature(uint16_t pin){
+    ADC0.init(pin); //Activates ADC on port D with initialized pin
+    uint16_t adcVal = ADC0_read();  //Reads Analog value on input
+    uint16_t TemperaturRead = temperaturC(adcVal, 0);   //converts A-value to temperature
+    return TemperaturRead;
+}
