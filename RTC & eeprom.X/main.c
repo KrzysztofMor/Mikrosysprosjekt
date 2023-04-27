2c9b5aa205a57d8dc80843252ffed18a2da8917a
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-04-27 09:40:00 +0200

    Update main.c
    komprimert

--- RTC & eeprom.X/main.c
+++ RTC & eeprom.X/main.c
@@ -13,27 +13,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "RTC.h"
-
+void RuntimeStartup();
 
 int main() {
     USART3_init();
+    RuntimeStartup();
     PORTB_DIRCLR = PIN2_bm; //A button can be used to reset runtime.
-    RTC_init(); //Starts counter
-    fan_memory();   //reads stored fan runtime from eeprom
-    sei();  //enables interrupts 
 
     while(1){
         uint8_t x = get_runtime_fan(0); //get runtime from one of the fans
         printf("fan1: %u\n", x);    //serialprint runtime
-        _delay_ms(1000);  
-        
-        //reset_fan_runtime(0);         //Reset fancount when fan is replaced
+        _delay_ms(1000);              
         
         if (!(PORTC.IN & (PIN2_bm))) {  //Use to enable reset on buttonpress
-        //    reset_fan_runtime(0);
+            reset_fan_runtime(0);   //Reset fancount when fan is replaced
         }
-    }
-    
-    
+    }   
 }
 
+void RuntimeStartup(){
+    RTC_init(); //Starts counter
+    fan_memory();   //reads stored fan runtime from eeprom
+    sei();  //enables interrupts 
+}
