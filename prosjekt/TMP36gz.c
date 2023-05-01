99e2f2f80f25bacdc71836cdbc354e08048540b9
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-05-01 15:04:55 +0200

    Update TMP36gz.c

--- prosjekt/TMP36gz.c
+++ prosjekt/TMP36gz.c
@@ -10,10 +10,24 @@
 uint16_t temperaturC(uint16_t temp, uint8_t tempScale);
 uint8_t analogTempvalue(uint8_t temperature, uint8_t min, uint8_t max);
 uint8_t map(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max );
-//uint16_t read_i2c_temp(uint16_t pin, uint16_t minimum, uint16_t maximum);
 uint16_t read_fantemperature(uint16_t pin);
 
-//case 0 is Celsius, case 1 is Fahrenheit, case 2 is Kelvin
+/*
+Reads value from analog pin and returns value as a temperature.
+Tempscale set to celsius
+*/
+uint16_t read_fantemperature(uint16_t pin){
+    ADC0_init(pin); //Activates ADC on port D and C with initialized pin
+    uint16_t adcVal = ADC0_read();  //Reads Analog value on input
+    uint16_t TemperaturRead = temperaturC(adcVal, 0);   //converts A-value to temperature
+    return TemperaturRead;
+}
+
+/*
+Takes in analog value from TMP36gz sensor, and calculate temperature.
+For international use, you are free to choose whatever scale you want.
+case 0 is Celsius, case 1 is Fahrenheit, case 2 is Kelvin
+*/
 uint16_t temperaturC(uint16_t temp, uint8_t tempScale){
     uint8_t c = (temp-500)/10;  //equation for TMP36gz sensor. Value is celcius.
     switch(tempScale){
@@ -55,21 +69,7 @@ uint8_t analogTempvalue(uint8_t temperature, uint8_t min, uint8_t max){
 /*
  map converts a value from one scale, over to a different scale.
  */
-uint8_t map(uint8_t x , uint8_t in_min , uint8_t in_max , uint8_t out_min , uint8_t out_max ) {
+long map(long x , long in_min , long in_max , long out_min , long out_max ) {
     return (x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min ;
 }  
-/*
-uint16_t read_i2c_temp(uint16_t pin, uint16_t minimum, uint16_t maximum){
-    ADC0_init(pin); //Activates ADC on port D with initialized pin
-    uint16_t adcVal = ADC0_read();  //Reads Analog value on input
-    uint16_t TemperaturRead = temperaturC(adcVal, 0);   //converts A-value to temperature
-    uint16_t analogValue = analogTempvalue(TemperaturRead, minimum, maximum);   //Converts Temperatur to analog value with minimum and maximum range. 
-    return analogValue;
-}
-*/
-uint16_t read_fantemperature(uint16_t pin){
-    ADC0_init(pin); //Activates ADC on port D and C with initialized pin
-    uint16_t adcVal = ADC0_read();  //Reads Analog value on input
-    uint16_t TemperaturRead = temperaturC(adcVal, 0);   //converts A-value to temperature
-    return TemperaturRead;
-}
+
