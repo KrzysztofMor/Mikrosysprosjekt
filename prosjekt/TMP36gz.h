b3a664f2d27c06886e972b726f9d899bf872e9e2
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-05-01 15:10:59 +0200

    Update TMP36gz.h

--- prosjekt/TMP36gz.h
+++ prosjekt/TMP36gz.h
@@ -7,10 +7,16 @@
 
 #pragma once
 
+//Converts analogvalue to a temperature value
 uint16_t temperaturC(uint16_t temp, uint8_t tempScale);
+
+//converts temperature value to dutycycle values for PWM signal
 uint8_t analogTempvalue(uint8_t temperature, uint8_t min, uint8_t max);
-//uint16_t read_i2c_temp(uint16_t pin, uint16_t minimum, uint16_t maximum);
+
+//Reads temperature on chosen fanpin
 uint16_t read_fantemperature(uint16_t pin);
-uint8_t map(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max );
+
+//Converts a value on one scale over to another scale
+long map(long x, long in_min, long in_max, long out_min, long out_max );
 
 
