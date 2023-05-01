4be18a921f3a8d0c9f661878ad6086853e938e92
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-05-01 16:26:38 +0200

    Update rpm.h

--- prosjekt/rpm.h
+++ prosjekt/rpm.h
@@ -1,8 +1,22 @@
+/* 
+ * File:   rpm.h
+ * Author: ERIKG
+ * Created on April 19, 2023, 5:36 PM
+ */
+
 #pragma once
 
+//Activates capture mode on TCB
 void readRPM_init();
+
+//Reads values from TCB and calculate RPM
 uint32_t readRPM();
+
+//choose which pin should be red
 void RPMpin(uint32_t pin);
+
+//compares dutycycle of output signal with input signal
 //uint8_t compare_PWM(uint16_t TCA_CMP);
+
+//runs pwm signal with custom duty cycle
 void pwm_ut(uint8_t duty);
-//uint32_t readfanspeed(uint32_t pin);
