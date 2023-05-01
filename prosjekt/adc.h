b4b5410b917a9849689df1dfc8ab63813f4ca91d
eriklgil <113515048+eriklgil@users.noreply.github.com>
2023-05-01 15:36:39 +0200

    Update adc.h

--- prosjekt/adc.h
+++ prosjekt/adc.h
@@ -1,12 +1,17 @@
 /*
-*  adc0 pin6
+*  ADC0
 *
 *  created: 14.04.23
 *  Author: Erik Gilberg
 */
 #pragma once
 
+//initializing ADC on interrupt
 void ADC0_init(uint8_t port);
+
+//Reads signal from last interrupt
 uint16_t ADC0_read(void);
+
+//Choose what analog input should be red
 void ADC0_channels(uint8_t port);
 
